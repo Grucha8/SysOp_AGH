26f48ba11f6bb7f4d2a199fe6810b381b39949f6
Grucha8 <t.szamot1997@gmail.com>
2018-06-06 19:45:39 +0200

    completed cw09/zad1

--- cw09/zad1/main.c
+++ cw09/zad1/main.c
@@ -1,20 +1,21 @@
-/* ====INCLUDE BLOCK==== */
+/* ============INCLUDE BLOCK============ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 
-/* ====END OF INCLUDE BLOCK==== */
+/* ============END OF INCLUDE BLOCK============ */
 
 
-/* ====DEFINE BLOCK==== */
+/* ============DEFINE BLOCK============ */
 #define ERROR_MSG(format, ...) { printf(format, ##__VA_ARGS__); exit(-1);}
 
-/* ====END OF DEFINE BLOCK==== */
+/* ============END OF DEFINE BLOCK============ */
 
 /* enum */
 enum data_status{
-    ISPLACE = 0, FULL = 1
+    NOTFULL = 0, FULL = 1
 };
 
 enum which_searching{
@@ -38,36 +39,24 @@ struct thread_arg{
 };
 
 
-/* ====GLOBAL VARIABLES BLOCK==== */
+/* ============GLOBAL VARIABLES BLOCK============ */
 struct shared_memory_data data;
-static FILE* dataFile;
+FILE* dataFile;
 pthread_mutex_t mutex;
 pthread_cond_t cond;
 
-/* ====END OF GLOBAL VARIABLES BLOCK==== */
+/* ============END OF GLOBAL VARIABLES BLOCK============ */
 
 
-/* ====FUNCTION DECLARATIONS BLOCK==== */
+/* ============FUNCTION DECLARATIONS BLOCK============ */
 void init(char* fileName, struct thread_arg* tArgs);
-void at_exit(void);
-void threads(struct thread_arg* tArgs);
 void* cus_function(void* arg);
 void* man_function(void* arg);
 
-/* ====END OF FUNCTION DECLARATION BLOCK==== */
-
-void fill_data(struct thread_arg* a, struct thread_arg b){
-    a->N = b.N;
-    a->K = b.K;
-    a->L = b.L;
-    a->nk = b.nk;
-    a->searching = b.searching;
-    a->P = b.P;
-    a->writingFlag = b.writingFlag;
-}
+/* ============END OF FUNCTION DECLARATION BLOCK============ */
 
 
-/* ====FUNCTIONS==== */
+/* ============FUNCTIONS============ */
 int main(int argc, char* argv[]){
     if (argc < 2)
         ERROR_MSG("Too few arguments\n");
@@ -77,33 +66,40 @@ int main(int argc, char* argv[]){
     init(argv[1], &tArgs);
 
     pthread_t man[tArgs.P], cus[tArgs.K];
-   // struct thread_arg mData[tArgs.P], cData[tArgs.K];
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
 
-    printf("%i --\n", tArgs.P);
     for (int i = 0; i < tArgs.P; ++i) {
-        printf("%i ss\n", i);
-
         if (pthread_create(&man[i], &attr, man_function, (void *) &tArgs) == -1)
         ERROR_MSG("Couldn't create a thread\n");
     }
+   // sleep(1);
     for (int i = 0; i < tArgs.K; ++i) {
-
         if (pthread_create(&cus[i], &attr, cus_function, (void *) &tArgs) == -1)
         ERROR_MSG("Couldn't create a thread\n");
     }
 
     pthread_attr_destroy(&attr);
 
-    for (int i = 0; i < tArgs.P; ++i) {
-        pthread_join(man[i], NULL);
-    }
-    for (int i = 0; i < tArgs.K; ++i) {
-        pthread_join(cus[i], NULL);
+    if(tArgs.nk > 0) {
+        sleep(tArgs.nk);
+
+        for (int j = 0; j < tArgs.P; ++j) {
+            pthread_cancel(man[j]);
+        }
+        for (int i = 0; i < tArgs.K; ++i) {
+            pthread_cancel(cus[i]);
+        }
+    }else if (tArgs.nk == 0){
+        for (int j = 0; j < tArgs.P; ++j) {
+            pthread_join(man[j], NULL);
+        }
+        for (int i = 0; i < tArgs.K; ++i) {
+            pthread_cancel(cus[i]);
+        }
     }
 }
 
@@ -130,9 +126,10 @@ void init(char* fileName, struct thread_arg* tArgs){
 
     if (getline(&buf, &len, pFILE) == -1)
         ERROR_MSG("No dataFile\n");
-    buf[strlen(buf)] = '\0';
-    //if ((dataFile = fopen(buf, "r")) == -1)
-      //  ERROR_MSG("hcuasdasd\n");
+    buf[strlen(buf)-1] = '\0';
+    if ((dataFile = fopen(buf, "r")) == NULL)
+        ERROR_MSG("Coudln't open pan_tadeusz\n");
+
 
     if (getline(&buf, &len, pFILE) == -1)
         ERROR_MSG("No L\n");
@@ -140,7 +137,6 @@ void init(char* fileName, struct thread_arg* tArgs){
 
     if (getline(&buf, &len, pFILE) == -1)
     ERROR_MSG("No searching_type\n");
-    //buf[strlen(buf)] = '\0';
     switch (buf[0]){
         case '=':
             tArgs->searching = EQUAL;
@@ -167,7 +163,7 @@ void init(char* fileName, struct thread_arg* tArgs){
     data.dataArray = (char**)calloc(tArgs->N, sizeof(char*));
     data.customerPosition = 0;
     data.manufacturerPosition = 0;
-    data.status = ISPLACE;
+    data.status = NOTFULL;
 
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&cond, NULL);
@@ -175,183 +171,139 @@ void init(char* fileName, struct thread_arg* tArgs){
     fclose(pFILE);
 }
 
-
-/* ====MANUFACTURER BLOCK==== */
-void parse_line(int whichBlock){
-    char* tmp;
-    size_t size;
+/* ========================================================== */
+/* ====================MANUFACTURER BLOCK==================== */
+/* ========================================================== */
+void parse_line(int whichBlock, int writingFlag){
+    char* tmp = NULL;
+    size_t size = 0;
     int len;
-   // printf("MUTEX1\n");
 
-   // printf("MUTEX2\n");
-    FILE* huj = fopen("pan_tadeusz.txt", "r");
-    getline(&tmp, &size, huj);
-    fclose(huj);
-
-    len = strlen(tmp);
-    printf("%s\n", tmp);
-
-   // printf("MUTEX3\n");
-    if(tmp[len-1] == '\n') {
-        tmp[len-1] = '\0';
-        len--;
-
-     //   printf("MUTEX4\n");
+    if(getline(&tmp, &size, dataFile) < 0) {
+        pthread_mutex_unlock(&mutex);
+        pthread_exit((void *) 0);
     }
 
+    len = strlen(tmp);
 
-    data.dataArray[whichBlock] = calloc(len, sizeof(char));
+    if (writingFlag)
+        printf("Manufacturer: adding to data:<<%s>>", tmp);
+
+    data.dataArray[whichBlock] = calloc(len+1, sizeof(char));
     strcpy(data.dataArray[whichBlock], tmp);
 }
 
-int is_a_free_slot(int i, int N){
-    //int i = data.manufacturerPosition;
-    do {
-        if (i == N)
-            i = 0;
-
-        if (data.dataArray[i] == NULL || data.dataArray[i][0] == 0) {
-            printf("=======i: %i=======\n", i);
-            return i;             // found an empty slot
-        }
-        i++;
-    } while (i != data.manufacturerPosition);
-
-    return -1;
-}
 
 void* man_function(void* arg){
     struct thread_arg* my_data;
     my_data = (struct thread_arg*) arg;
 
-    struct timespec start;
-    struct timespec now;
-    clock_gettime(CLOCK_REALTIME, &start);          // time in which thread started
+    if (my_data->writingFlag)
+        printf("Manufacturer: starring work\n");
 
     while (1) {
         pthread_mutex_lock(&mutex);
 
+        if (my_data->writingFlag)
+            printf("Manufacturer: getting mutex\n");
 
-        int whichBlock;
-        // waiting for an empty pos
-        while ((whichBlock = is_a_free_slot(data.manufacturerPosition, my_data->N)) == -1){
-            clock_gettime(CLOCK_REALTIME, &now);
-
-            // time has ended
-            if (my_data->nk > 0 && now.tv_sec - start.tv_sec > my_data->nk){
-                pthread_mutex_unlock(&mutex);
-                return (void*) 0;
-            }
-
+        // checking if full
+        while (data.status == FULL){
             pthread_cond_wait(&cond, &mutex);
         }
 
+        // manufacturer is adding new line
+        int whichBlock = (data.manufacturerPosition);
 
         // putting line into data array
-        parse_line(whichBlock);
-        data.manufacturerPosition = whichBlock;
-        if(data.status == FULL){
-            data.status = ISPLACE;
-            pthread_cond_broadcast(&cond);
+        parse_line(whichBlock, my_data->writingFlag);
 
+        if ((data.manufacturerPosition+1)%my_data->N == data.customerPosition){
+            data.status = FULL;
+        }
+        if ((data.customerPosition) == data.manufacturerPosition){
+            pthread_cond_broadcast(&cond);      // if man added product next on the cus list
         }
 
+        data.manufacturerPosition = (whichBlock + 1) % my_data->N;
 
+        if (my_data->writingFlag)
+            printf("Manufacturer: giving away mutex\n");
         pthread_mutex_unlock(&mutex);
     }
 }
-/* ====END OF MANUFACTURER BLOCK==== */
+/* ============END OF MANUFACTURER BLOCK============ */
 
 
-/* ====CUSTOMER BLOCK==== */
-int first_not_empty_block(int i, int N){
-    do {
-        if (i == N) {
-            i = 0;
-            if (i == data.customerPosition)
-                break;
-        }
-
-       // printf("i: %i\n", i);
-        if (data.dataArray[i] != NULL) {
-            printf("HUJEX1\n");
-            return i;             // found an not empty slot
-        }
-        i++;
-    } while (1);
-
-    return -1;
-}
+/* ============CUSTOMER BLOCK============ */
 
 void write_string(int whichBlock, int searching, int maxSize){
     char tmp[strlen(data.dataArray[whichBlock])+1];
+
     strcpy(tmp, data.dataArray[whichBlock]);
+
     free(data.dataArray[whichBlock]);
     data.dataArray[whichBlock] = NULL;
 
-    printf("wypisywanie:\n");
     switch (searching){
         case EQUAL:
             if (strlen(tmp) == maxSize)
-                printf("%s\n", tmp);
+                printf("%s", tmp);
             break;
         case GREATER:
             if (strlen(tmp) > maxSize)
-                printf("%s\n", tmp);
+                printf("%s", tmp);
             break;
         case SMALLER:
             if (strlen(tmp) < maxSize)
-                printf("%s\n", tmp);
+                printf("%s", tmp);
             break;
         default:
             printf("ERROR\n");
     }
 
-    //free(data.dataArray[whichBlock]);
-    //free(tmp);
 
+   // free(tmp);
 }
 
 void* cus_function(void *arg){
     struct thread_arg* my_data;
     my_data = (struct thread_arg*) arg;
-    printf("customer %i!!\n", my_data->N);
 
-    struct timespec start, now;
-    clock_gettime(CLOCK_REALTIME, &start);          // time in which thread started
+    if (my_data->writingFlag)
+        printf("Customer: starting work\n");
 
     while (1){
         pthread_mutex_lock(&mutex);
 
-        int whichBlock;
 
+        if (my_data->writingFlag)
+            printf("Customer: getting mutex\n");
 
-        // waiting for an empty pos
-        while ((whichBlock = first_not_empty_block(data.customerPosition, my_data->N)) == -1){
-            clock_gettime(CLOCK_REALTIME, &now);
-
-           // printf("kurwa:%ld\n", now.tv_sec);
-            // check if the time has ended
-            if (my_data->nk > 0 && now.tv_sec - start.tv_sec > my_data->nk){
-                pthread_mutex_unlock(&mutex);
-                return (void*) 0;
-            }
-
+        // waiting for an item
+        while (data.customerPosition == data.manufacturerPosition && data.status == NOTFULL){
             pthread_cond_wait(&cond, &mutex);
         }
 
-            printf("%i \n", whichBlock);
-            // deleting a block
-            write_string(whichBlock, my_data->searching, my_data->L);
-            printf("hujex2\n");
-            data.customerPosition = whichBlock;
-            data.action = DELETED;
-            pthread_cond_broadcast(&cond);
+        int whichBlock = (data.customerPosition);
 
+        // deleting a block
+        write_string(whichBlock, my_data->searching, my_data->L);
+        data.customerPosition = (whichBlock+1) % my_data->N;
+
+        // if array was full then broadcast
+        if (data.status == FULL){
+            data.status = NOTFULL;
+            pthread_cond_broadcast(&cond);
+        }
+
+
+        if (my_data->writingFlag)
+            printf("Customer: giving away mutex\n");
 
         pthread_mutex_unlock(&mutex);
     }
 
 }
 
-/* ====END OF CUSTOMER BLOCK==== */
+/* ============END OF CUSTOMER BLOCK============ */
